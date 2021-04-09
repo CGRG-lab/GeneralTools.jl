1ff22464aa371b04f22033a9350ead568bccf970
okatsn <49422823+okatsn@users.noreply.github.com>
2021-04-09 10:13:37 +0800

    Update back2.jl

--- src/back2.jl
+++ src/back2.jl
@@ -4,9 +4,9 @@ return back2_core(this_path, n)
 end
 
 # go back to the ith parent directory and precede to directory_ahead
-function back2(this_path, n, directory_ahead)
+function back2(this_path, n, directory_ahead...)
 this_path = back2_core(this_path, n);
-return joinpath(this_path,directory_ahead)
+return joinpath(this_path,directory_ahead...)
 end
 
 function back2_core(this_path, n)
