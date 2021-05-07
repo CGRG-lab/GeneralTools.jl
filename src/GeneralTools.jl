3471bb192f07f0f858f58be04a15874d9ac1c550
okatsn <49422823+okatsn@users.noreply.github.com>
2021-05-07 15:10:05 +0800

    create getext.jl

--- src/GeneralTools.jl
+++ src/GeneralTools.jl
@@ -1,6 +1,10 @@
  # usually git ignore manifest if this is a package for using, 
  # because usually dependencies on a specific version of other package (e.g. Plots) is not necessary, 
  # and if Manifest.toml is uploaded to github, people may have to install two different versions of Plots, resulting in confliction.
+ 
+ """
+ You can add GeneralTools at https://github.com/CGRG-lab/GeneralTools.jl.git
+ """
 module GeneralTools 
     filesep = Base.Filesystem.path_separator;
     export filesep
@@ -14,6 +18,7 @@ module GeneralTools
 	include("circles.jl");
 	include("findfirstfromlast.jl");
 	include("cdfE.jl")
+	include("getext.jl");
 	using Plots # you have to add Plots under this environment, i.e. (GeneralTools) pkg> add Plots
 	# supertitle will fail without using Plots here, 
 	# even when `using Plots` has been applied in your script.
@@ -25,5 +30,6 @@ module GeneralTools
 	export openit
 	export findfirstfromlast
 	export cdfE
+	export getext
 	
 end
---
+++ src/getext.jl
@@ -0,0 +1,13 @@
+"""
+split the input name into the [name,".",extension] array.
+For example:
+	pathsplit = getext("inputfpath\\file.ext")
+    pathsplit[1] = pathsplit[1]*"_copy";
+	outputpath = join(pathsplit); # which returns "inputfpath\\file_copy.ext"
+"""
+function getext(pathname)
+dotpos = findall(isequal('.'),pathname);
+str_ext = pathname[(dotpos[end]+1):length(pathname)];
+str_beforedot = pathname[1:(dotpos[end]-1)];
+return [str_beforedot,".",str_ext]
+end
